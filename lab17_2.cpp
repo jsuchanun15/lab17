46ca9bffef37ff41815e1dec6b19772f06e788ac
jsuchanun15 <j.suchanan15@gmail.com>
2025-02-07 01:51:01 +0700

    Update lab17_2.cpp

--- lab17_2.cpp
+++ lab17_2.cpp
@@ -1,19 +1,7 @@
-#include<iostream>
-using namespace std;
-
-________ myString(_____________________){
-	_______ = new ____________;    
-	for(int i = 0; i < N;i++) ____________ = 'A'+i;
-	__________ = 0;
-}
-
-int main(){
-	char *p;
-	int N;	
-	cout << "N = ";
-	cin >> N;		
-	myString(p,N);
-	cout << p;	
-	delete [] p;
-	return 0;
+void myString(char *&str, int N){
+	str = new char[N + 1];    
+	for(int i = 0; i < N;i++){
+        str[i] = 'A' + i;  
+    }
+    str[N] = '\0'; 
 }
